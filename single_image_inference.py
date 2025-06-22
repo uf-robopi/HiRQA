48198d7f30e8f440a19fe376148af4bb2cee02d9
Vaishnav Ramesh <vaishnavramesh@login7.ufhpc>
2025-06-22 17:56:30 -0400

    Update single_image_inference

--- single_image_inference.py
+++ single_image_inference.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate IQA scores for images in a folder.')
     parser.add_argument('--img_path', type=str, required=True, help='Image Path')   
     parser.add_argument('--model', type=str, default="Resnet", help='model choice[HiRQA/HiRQA-S]')
-    parser.add_argument("--ckpt", type=str, default="", help="Path to a checkpoint to resume from")
+    parser.add_argument("--ckpt", type=str, default="", help="Path to model weights")
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
